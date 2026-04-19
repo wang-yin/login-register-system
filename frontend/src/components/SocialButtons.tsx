b634e8b3c9563322db147a24495ec3e34615ce8c
Wang-yin <sky06456@gmail.com>
2026-04-20 02:32:06 +0800

    refactor(frontend): 將第三方登入連結改為環境變數

--- frontend/src/components/SocialButtons.tsx
+++ frontend/src/components/SocialButtons.tsx
@@ -2,16 +2,18 @@ import { FaGithub } from 'react-icons/fa';
 import { FaGooglePlusG } from 'react-icons/fa';
 
 export default function SocialButtons() {
+  const API_BASE_URL =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
   return (
     <>
       <a
-        href="http://localhost:5000/api/auth/github"
+        href={`${API_BASE_URL}/auth/github`}
         className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border"
       >
         <FaGithub size={25} />
       </a>
       <a
-        href="http://localhost:5000/api/auth/google"
+        href={`${API_BASE_URL}/auth/google`}
         className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border"
       >
         <FaGooglePlusG size={25} />
